15ddf3e7487c8e440ec189a771ea12a37eb00781
pheest <pheest@9c91d93d-f138-4f03-b554-451257969c6d>
2013-12-23 17:28:23 +0000

    git-svn-id: http://fussvn.fusion.ccfe.ac.uk/svnroot/EPICS@125 9c91d93d-f138-4f03-b554-451257969c6d

--- pcAccessCPP/testApp/runtests.bat
+++ pcAccessCPP/testApp/runtests.bat
@@ -7,21 +7,21 @@ set EPICS_HOST_ARCH=win32-x86
 :continue
 
 REM client
-bin\%EPICS_HOST_ARCH%\testCreateRequest.exe
-bin\%EPICS_HOST_ARCH%\testStartStop.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testCreateRequest.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testStartStop.exe
 
 REM remote
-bin\%EPICS_HOST_ARCH%\testGetPerformance.exe
-start bin\%EPICS_HOST_ARCH%\testBlockingTCPSrv.exe
-bin\%EPICS_HOST_ARCH%\testBlockingTCPClnt.exe
-start bin\%EPICS_HOST_ARCH%\testBlockingUDPSrv.exe
-bin\%EPICS_HOST_ARCH%\testBlockingUDPClnt.exe
-start bin\%EPICS_HOST_ARCH%\testServer.exe
-bin\%EPICS_HOST_ARCH%\testChannelConnect.exe
-bin\%EPICS_HOST_ARCH%\testRemoteClientImpl.exe
-bin\%EPICS_HOST_ARCH%\testServerContext.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testGetPerformance.exe
+start %~dp0..\bin\%EPICS_HOST_ARCH%\testBlockingTCPSrv.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testBlockingTCPClnt.exe
+start %~dp0..\bin\%EPICS_HOST_ARCH%\testBlockingUDPSrv.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testBlockingUDPClnt.exe
+start %~dp0..\bin\%EPICS_HOST_ARCH%\testServer.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testChannelConnect.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testRemoteClientImpl.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testServerContext.exe
 
 REM utils
-bin\%EPICS_HOST_ARCH%\testAtomicBoolean.exe
-bin\%EPICS_HOST_ARCH%\testHexDump.exe
-bin\%EPICS_HOST_ARCH%\testInetAddressUtils.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testAtomicBoolean.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testHexDump.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testInetAddressUtils.exe
