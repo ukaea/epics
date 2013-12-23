78bff3464e8b7623aee2b5dfe5060f0e0acf94b4
pheest <pheest@9c91d93d-f138-4f03-b554-451257969c6d>
2013-12-23 17:12:50 +0000

    git-svn-id: http://fussvn.fusion.ccfe.ac.uk/svnroot/EPICS@10 9c91d93d-f138-4f03-b554-451257969c6d

---
+++ base/src/libCom/osi/osiProcess.h
@@ -0,0 +1,46 @@
+/*************************************************************************\
+* Copyright (c) 2002 The University of Chicago, as Operator of Argonne
+*     National Laboratory.
+* Copyright (c) 2002 The Regents of the University of California, as
+*     Operator of Los Alamos National Laboratory.
+* EPICS BASE Versions 3.13.7
+* and higher are distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution. 
+\*************************************************************************/
+
+/* 
+ * Revision-Id: anj@aps.anl.gov-20101005192737-disfz3vs0f3fiixd
+ * 
+ * Operating System Independent Interface to Process Environment
+ *
+ * Author: Jeff Hill
+ *
+ */
+#include "shareLib.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum osiGetUserNameReturn {
+                osiGetUserNameFail, 
+                osiGetUserNameSuccess} osiGetUserNameReturn;
+epicsShareFunc osiGetUserNameReturn epicsShareAPI osiGetUserName (char *pBuf, unsigned bufSize);
+
+/*
+ * Spawn detached process with named executable, but return 
+ * osiSpawnDetachedProcessNoSupport if the local OS does not
+ * support heavy weight processes.
+ */
+typedef enum osiSpawnDetachedProcessReturn {
+                osiSpawnDetachedProcessFail, 
+                osiSpawnDetachedProcessSuccess,
+                osiSpawnDetachedProcessNoSupport} osiSpawnDetachedProcessReturn;
+
+epicsShareFunc osiSpawnDetachedProcessReturn epicsShareAPI osiSpawnDetachedProcess 
+    (const char *pProcessName, const char *pBaseExecutableName);
+
+#ifdef __cplusplus
+}
+#endif
+
