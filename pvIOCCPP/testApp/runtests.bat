7010640ff3a96a5d87cde09e4c3d2a04c449f122
pheest <pheest@9c91d93d-f138-4f03-b554-451257969c6d>
2013-12-23 17:28:35 +0000

    git-svn-id: http://fussvn.fusion.ccfe.ac.uk/svnroot/EPICS@131 9c91d93d-f138-4f03-b554-451257969c6d

--- pvIOCCPP/testApp/runtests.bat
+++ pvIOCCPP/testApp/runtests.bat
@@ -6,5 +6,5 @@ set EPICS_HOST_ARCH=win32-x86
 
 :continue
 
-start bin\%EPICS_HOST_ARCH%\exampleServiceRPC.exe
-bin\%EPICS_HOST_ARCH%\exampleClient.exe
+start %~dp0..\bin\%EPICS_HOST_ARCH%\exampleServiceRPC.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\exampleClient.exe
