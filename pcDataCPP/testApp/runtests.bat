a489b5ad7facbb1b1a8f0ec095cb8ac4c4cd0d46
pheest <pheest@9c91d93d-f138-4f03-b554-451257969c6d>
2013-12-24 11:57:11 +0000

    git-svn-id: http://fussvn.fusion.ccfe.ac.uk/svnroot/EPICS@341 9c91d93d-f138-4f03-b554-451257969c6d

--- pcDataCPP/testApp/runtests.bat
+++ pcDataCPP/testApp/runtests.bat
@@ -7,34 +7,34 @@ set EPICS_HOST_ARCH=win32-x86
 :continue
 
 REM misc
-bin\%EPICS_HOST_ARCH%\testBaseException.exe
-bin\%EPICS_HOST_ARCH%\testBitSet.exe
-bin\%EPICS_HOST_ARCH%\testByteBuffer.exe
-bin\%EPICS_HOST_ARCH%\testByteOrder.exe
-bin\%EPICS_HOST_ARCH%\testMessageQueue.exe
-bin\%EPICS_HOST_ARCH%\testQueue.exe
-bin\%EPICS_HOST_ARCH%\testSerialization.exe
-bin\%EPICS_HOST_ARCH%\testThread.exe
-bin\%EPICS_HOST_ARCH%\testTimer.exe
-bin\%EPICS_HOST_ARCH%\testTimeStamp.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testBaseException.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testBitSet.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testByteBuffer.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testByteOrder.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testMessageQueue.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testQueue.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testSerialization.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testThread.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testTimer.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testTimeStamp.exe
 
 REM monitor
-bin\%EPICS_HOST_ARCH%\testMonitor.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testMonitor.exe
 
 REM property
-bin\%EPICS_HOST_ARCH%\testProperty.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testProperty.exe
 
 REM pv
 
-bin\%EPICS_HOST_ARCH%\testBitSetUtil.exe
-bin\%EPICS_HOST_ARCH%\testConvert.exe
-bin\%EPICS_HOST_ARCH%\testIntrospect.exe
-bin\%EPICS_HOST_ARCH%\testOperators.exe
-bin\%EPICS_HOST_ARCH%\testPVAppend.exe
-bin\%EPICS_HOST_ARCH%\testPVAuxInfo.exe
-bin\%EPICS_HOST_ARCH%\testPVData.exe
-bin\%EPICS_HOST_ARCH%\testPVScalarArray.exe
-bin\%EPICS_HOST_ARCH%\testPVStructureArray.exe
-bin\%EPICS_HOST_ARCH%\testPVType.exe
-bin\%EPICS_HOST_ARCH%\testStandardField.exe
-bin\%EPICS_HOST_ARCH%\testStandardPVField.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testBitSetUtil.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testConvert.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testIntrospect.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testOperators.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testPVAppend.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testPVAuxInfo.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testPVData.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testPVScalarArray.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testPVStructureArray.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testPVType.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testStandardField.exe
+%~dp0..\bin\%EPICS_HOST_ARCH%\testStandardPVField.exe
