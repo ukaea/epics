459d6e201dba275b68ea8e53e08774b0a69a6cb9
pheest <pheest@9c91d93d-f138-4f03-b554-451257969c6d>
2014-10-29 18:53:01 +0000

    git-svn-id: http://fussvn.fusion.ccfe.ac.uk/svnroot/EPICS@529 9c91d93d-f138-4f03-b554-451257969c6d

---
+++ LeyboldTurbo/iocBoot/iocLeyboldTurbo/st.cmd
@@ -0,0 +1,32 @@
+#!../../bin/win32-x86/LeyboldTurbo
+
+## You may have to change LeyboldTurbo to something else
+## everywhere it appears in this file
+
+## Register all support components
+dbLoadDatabase ("../dbd/LeyboldTurbo.dbd")
+LeyboldTurbo_registerRecordDeviceDriver pdbbase
+
+epicsEnvSet ASYNPORT TURBO:$(INDEX)
+epicsEnvSet IOPORT PUMP:$(INDEX)
+
+# Configure asyn communication port, first
+drvAsynSerialPortConfigure($(IOPORT), $(COMPORT), 0, 0, 0)
+
+## Load record instances
+dbLoadRecords("db/LeyboldTurbo.db", "P=$(IOPORT):,PORT=$(IOPORT)")
+dbLoadRecords("../../asyn/db/asynRecord.db","P=$(IOPORT):,R=asyn1,PORT=$(IOPORT),ADDR=0,OMAX=80,IMAX=80")
+
+asynSetOption ($(IOPORT), 0, "baud", "19200")
+asynSetOption ($(IOPORT), 0, "bits", "8")
+asynSetOption ($(IOPORT), 0, "parity", "even")
+asynSetOption ($(IOPORT), 0, "stop", "1")
+asynSetOption($(IOPORT), -1, "clocal", "Y")
+asynSetOption($(IOPORT), -1, "crtscts", "N")
+
+LeyboldTurboPortDriverConfigure($(ASYNPORT), $(IOPORT))
+
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=pheestHost"
