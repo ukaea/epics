ae5fa36096cc3d5409319bb57e00c6fd4f9a6b78
pheest <pheest@9c91d93d-f138-4f03-b554-451257969c6d>
2014-10-31 10:59:42 +0000

    git-svn-id: http://fussvn.fusion.ccfe.ac.uk/svnroot/EPICS@531 9c91d93d-f138-4f03-b554-451257969c6d

--- base/startup/win32.bat
+++ base/startup/win32.bat
@@ -115,6 +115,7 @@ set PATH=%PATH%;%~dp0..\..\asyn\bin\%EPICS_HOST_ARCH%
 set PATH=%PATH%;%~dp0..\..\seq\bin\%EPICS_HOST_ARCH%
 set PATH=%PATH%;%~dp0..\..\busy\bin\%EPICS_HOST_ARCH%
 set PATH=%PATH%;%~dp0..\..\motor\bin\%EPICS_HOST_ARCH%
+set PATH=%PATH%;%~dp0..\..\LeyboldTurbo\bin\%EPICS_HOST_ARCH%
 set EPICS_ROOT=C:\EPICS
 
 REM ======================================================
