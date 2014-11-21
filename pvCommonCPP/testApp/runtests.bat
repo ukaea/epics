509342dacd7425c91a41b47e75a153dd7dd9344c
pheest <pheest@9c91d93d-f138-4f03-b554-451257969c6d>
2014-11-21 10:00:06 +0000

    git-svn-id: http://fussvn.fusion.ccfe.ac.uk/svnroot/EPICS@611 9c91d93d-f138-4f03-b554-451257969c6d

--- pvCommonCPP/testApp/runtests.bat
+++
@@ -1,9 +0,0 @@
-if "%EPICS_HOST_ARCH%"=="" goto setarch
-goto continue
-
-:setarch
-set EPICS_HOST_ARCH=win32-x86
-
-:continue
-
-%~dp0..\bin\%EPICS_HOST_ARCH%\mb_test.exe
--- pvaPy/src/pvaccess/PvaExceptionTranslator.cpp
+++
@@ -1,8 +0,0 @@
-#include "PvaExceptionTranslator.h"
-#include "boost/python.hpp"
-
-void PvaExceptionTranslator::translator(const PvaException& ex)
-{
-    PyErr_SetString(PyExc_UserWarning, ex.what());
-}
-
