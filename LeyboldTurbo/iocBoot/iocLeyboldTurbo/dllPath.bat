a81701a3b9bd35f72030b73ac73211dbf87ee405
pheest <pheest@9c91d93d-f138-4f03-b554-451257969c6d>
2014-11-27 16:37:56 +0000

    git-svn-id: http://fussvn.fusion.ccfe.ac.uk/svnroot/EPICS@623 9c91d93d-f138-4f03-b554-451257969c6d

--- LeyboldTurbo/iocBoot/iocLeyboldTurbo/dllPath.bat
+++ LeyboldTurbo/iocBoot/iocLeyboldTurbo/dllPath.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-PATH %PATH%;D:/EPICS/LeyboldTurbo/bin/win32-x86;D:\EPICS/asyn/bin/win32-x86;D:\EPICS/base/bin/win32-x86
+PATH %PATH%;D:/EPICS/LeyboldTurbo/bin/win32-x86;D:/EPICS/asyn/bin/win32-x86;D:/EPICS/base/bin/win32-x86
