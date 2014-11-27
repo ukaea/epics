00a817525fe8c7776887f7c7eec4eefa4738a274
pheest <pheest@9c91d93d-f138-4f03-b554-451257969c6d>
2014-11-27 16:35:56 +0000

    git-svn-id: http://fussvn.fusion.ccfe.ac.uk/svnroot/EPICS@622 9c91d93d-f138-4f03-b554-451257969c6d

---
+++ LeyboldTurbo/LeyboldSimApp/LeyboldSim.bat
@@ -0,0 +1,15 @@
+echo off
+
+set EPICS_CA_SERVER_PORT=5071
+
+set IPPORT1=localhost:5066
+
+set IPPORT2=localhost:5067
+
+set IPPORT3=localhost:5068
+
+set IPPORT4=localhost:5069
+
+set IPPORT5=localhost:5070
+
+LeyboldSim.exe ..\iocBoot\iocLeyboldTurbo\st1.simulator.cmd
---
+++ LeyboldTurbo/LeyboldTurboApp/LeyboldTurbo.bat
@@ -0,0 +1,13 @@
+echo off
+
+set COMPORT1=COM1:
+set COMPORT2=COM2:
+set COMPORT3=COM3:
+set COMPORT4=COM4:
+set COMPORT5=COM5:
+set IPPORT1=localhost:5066
+set IPPORT2=localhost:5067
+set IPPORT3=localhost:5068
+set IPPORT4=localhost:5069
+set IPPORT5=localhost:5070
+LeyboldTurbo.exe ..\iocBoot\iocLeyboldTurbo\st1.simulated.cmd
