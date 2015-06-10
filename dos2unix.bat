5ca295fa35f09751163351a0cc6d9a81242c53da
pheest <Peter.Heesterman@ccfe.ac.uk>
2015-06-10 11:41:15 +0100

    Downloaded from Mark Rivers.

--- dos2unix.bat
+++ dos2unix.bat
@@ -1,8 +1,23 @@
 REM http://stackoverflow.com/questions/10989147/dos-batch-print-names-of-files-exclude-certain-extensions
 
+echo on
 
-FOR /D /R %%i IN (*) DO (
-	if "%%i" neq ".git" if "%%i" neq "boostApp" if "%%i" neq "Debug_DLL" if "%%i" neq "Debug_LIB" if "%%i" neq "Release_DLL" if "%%i" neq "Release_LIB" (
-		if "%%~i" neq ".bat" if "%%~i" neq ".sln" if "%%~i" neq ".vcxproj" if "%%~i" neq ".vcxproj.filters" if "%%~i" neq ".obj" dos2unix.exe %%i
-	)
+if "%1" neq "" pushd %1
+
+FOR %%j IN (*) DO (
+	echo %%j | findstr /i ".bat" > nul
+	if "%errorlevel%"=="0" goto skipit
+	echo %%j | findstr /i ".sln" > nul
+	if "%errorlevel%"=="0" goto skipit
+	echo %%j | findstr /i ".vcxproj" > nul
+	if "%errorlevel%"=="0" goto skipit
+	echo %%j | findstr /i "vcxproj.filters" > nul
+	if "%errorlevel%"=="0" goto skipit
+	dos2unix.exe %%j
+	goto continue
+:skipit
+	echo skipped "%%j"
+:continue
+	REM continuing
 )
+if "%1" neq "" popd
