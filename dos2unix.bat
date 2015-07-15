ce567f8dead8a51337b63d850bf7ff8cbc9bb7df
pheest <Peter.Heesterman@ccfe.ac.uk>
2015-07-15 17:20:31 +0100

    Put CR/LF the right way round

--- dos2unix.bat
+++ dos2unix.bat
@@ -1,23 +1,13 @@
 REM http://stackoverflow.com/questions/10989147/dos-batch-print-names-of-files-exclude-certain-extensions
 
-echo on
+echo off
+set ThisDirectory=%1
+if "%ThisDirectory%"=="" set ThisDirectory=%cd%
 
-if "%1" neq "" pushd %1
-
-FOR %%j IN (*) DO (
-	echo %%j | findstr /i ".bat" > nul
-	if "%errorlevel%"=="0" goto skipit
-	echo %%j | findstr /i ".sln" > nul
-	if "%errorlevel%"=="0" goto skipit
-	echo %%j | findstr /i ".vcxproj" > nul
-	if "%errorlevel%"=="0" goto skipit
-	echo %%j | findstr /i "vcxproj.filters" > nul
-	if "%errorlevel%"=="0" goto skipit
-	dos2unix.exe %%j
-	goto continue
-:skipit
-	echo skipped "%%j"
-:continue
-	REM continuing
+FOR %%j IN (%ThisDirectory%\*) DO (
+	if %%~xj neq .bat if %%~xj neq .sln if %%~xj neq .vcxproj if %%~xj neq .vcxproj.filters if %%~xj neq .sdf (
+		dos2unix.exe %%j
+	) else (
+		echo skipping %%j
+	)
 )
-if "%1" neq "" popd
