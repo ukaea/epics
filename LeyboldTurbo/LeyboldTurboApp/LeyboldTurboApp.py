eebe05768e31a3dd3144156407a68ccdccbebd33
pheest <Peter.Heesterman@ccfe.ac.uk>
2015-09-03 16:25:54 +0100

    Don't need VS user parameters for the device project

--- LeyboldTurbo/LeyboldTurboApp/LeyboldTurboApp.py
+++ LeyboldTurbo/LeyboldTurboApp/LeyboldTurboApp.py
@@ -1,3 +1,32 @@
+#####################################################################################################
+#																									#
+#	Module:																							#
+#		LeyboldTurboApp.py																			#
+#																									#
+#	Description:																					#
+#		Launches the IOC with appropriate parameter values.											#
+#		Script parameters are:																		#
+#			1. Number of pumps. Default 1.															#
+#			2. The size of the PZD area (see hardware documentation). Default 6.					#
+#				This will be 6 for older model controllers.											#
+#				It will be 2 for the rear port of the MAG Drive Digital pump.						#
+#				It will be 6 for the (undocumented) front port of the MAG Drive Digital.			#
+#				This also sets the baud rate.														#
+#				It will be 19200 for older model controllers.										#
+#				It will be 9600 for the rear port of the MAG Drive Digital.							#
+#				It will be 19200 for thew front port of the MAG Drive Digital.						#
+#			3. If a third parameter is passed, it means the IOC will work with the simulator,		#
+#				and not real hardware. Default is real.												#
+#		Pyepics is used for the convenience of cross-platform scripting.							#
+#																									#
+#	Author:  Peter Heesterman (Tessella plc). Date: 03 Sep 2015.									#
+#	Written for CCFE (Culham Centre for Fusion Energy).												#
+#																									#
+#	LeyboldTurbo is distributed subject to a Software License Agreement								#
+#	found in file LICENSE that is included with this distribution.									#
+#																									#
+#####################################################################################################
+
 import os
 import sys
 os.chdir(os.path.dirname(os.path.realpath(sys.argv[0])))
