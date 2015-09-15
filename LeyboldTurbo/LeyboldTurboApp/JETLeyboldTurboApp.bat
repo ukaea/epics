91d22c9751ef98c21ad13755b873d62a2d1f76f4
pheest <Peter.Heesterman@ccfe.ac.uk>
2015-09-15 15:58:02 +0100

    Correct asyn and com port mappings

--- LeyboldTurbo/LeyboldTurboApp/JETLeyboldTurboApp.bat
+++ LeyboldTurbo/LeyboldTurboApp/JETLeyboldTurboApp.bat
@@ -1,10 +1,26 @@
 set NUMPUMPS=6
 set BAUD=19200
 set NoOfPZD=2
+
+set ASYNPORT=TT
+
+set ASYNPORT1=TT:01
 set COMPORT1=COM1
+
+set ASYNPORT2=TT:02
 set COMPORT2=COM2
+
+set ASYNPORT3=TT:03
 set COMPORT3=COM3
+
+set ASYNPORT4=TT:04
 set COMPORT4=COM4
+
+set ASYNPORT5=NT:07
 set COMPORT5=COM7
+
+set ASYNPORT6=NT:07
 set COMPORT5=COM10
-..\Release_LIB\LeyboldTurboApp ..\iocBoot\iocLeyboldTurbo\st5.cmd
+
+
+..\Release_LIB\LeyboldTurboApp ..\iocBoot\iocLeyboldTurbo\st6.cmd
