52ac13b0852bb964047a0f554b9c996b4867a275
pheest <Peter.Heesterman@ccfe.ac.uk>
2015-10-14 17:35:14 +0100

    This script used in the vacuum group lab

---
+++ LeyboldTurbo/LeyboldTurboApp/Scripts/JET_Offline_LeyboldTurboApp.bat
@@ -0,0 +1,10 @@
+set NUMPUMPS=1
+set BAUD=9600
+set NoOfPZD=2
+
+set COMPORT1=COM2:
+
+start ..\..\Release_LIB\LeyboldTurboApp ..\..\iocBoot\iocLeyboldTurbo\st1.cmd
+
+md d:\LocalData\
+camonitor.py 1 1 D:\LocalData\
