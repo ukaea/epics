cc15f348b4d2e3ca96269958bcc230e11d0051f3
Heesterman, Peter J <Peter.Heesterman@ukaea.uk>
2020-02-28 11:44:57 +0000

    Commit tspd-adq with trigger time measured.

--- base-7.0.3.1/startup/win32.bat
+++ base-7.0.3.1/startup/win32.bat
@@ -93,6 +93,8 @@ goto ERREXIT
 :X64
 
 call "%PROGRAM_FILES%\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86_x64
+REM call "%PROGRAM_FILES%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_x64
+
 if "%1"=="x64-DLL" goto X64DLL
 if "%1"=="x64-debug" goto X64DEBUG
 if "%1"=="x64-debug-DLL" goto X64DEBUGDLL
@@ -117,6 +119,8 @@ goto CONTINUE
 REM --  win32-x86 ---
 :X86
 call "%PROGRAM_FILES%\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86
+
+REM call "%PROGRAM_FILES%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 if "%1"=="x86-DLL" goto X86DLL
 if "%1"=="x86-debug" goto X86DEBUG
 if "%1"=="x86-debug-DLL" goto X86DEBUGDLL
