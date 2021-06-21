a7382f0cf044ef598cfa59de688a2e626bdaf742
tim-speight <60938034+tim-speight@users.noreply.github.com>
2021-06-21 14:17:05 +0100

    further fixes to POST build vcxprojs

--- base-7.0.3.1/modules/database/src/tools/Build_DBDs.bat
+++ base-7.0.3.1/modules/database/src/tools/Build_DBDs.bat
@@ -1,12 +1,9 @@
-if not exist %TOPLEVEL%\lib\perl\DBD md %TOPLEVEL%\lib\perl\DBD
-if not exist %TOPLEVEL%\lib\perl\EPICS md %TOPLEVEL%\lib\perl\EPICS
 if not exist %TOPLEVEL%\lib md %TOPLEVEL%\lib
 if not exist %TOPLEVEL%\lib\perl md %TOPLEVEL%\lib\perl
+if not exist %TOPLEVEL%\lib\perl\DBD md %TOPLEVEL%\lib\perl\DBD
+if not exist %TOPLEVEL%\lib\perl\EPICS md %TOPLEVEL%\lib\perl\EPICS
 
-xcopy /Y /Q /D /R *.pl %DLLDIR%\ > NUL
-xcopy /Y /Q /D /R *.pm %TOPLEVEL%\lib\perl > NUL
-xcopy /Y /Q /D /R DBD\*.pm %TOPLEVEL%\lib\perl\DBD > NUL
-xcopy /Y /Q /D /R EPICS\*.pm %TOPLEVEL%\lib\perl\EPICS > NUL
-
-SET PERL5LIB=C:\Strawberry\lib\perl
-perl -CSD %DLLDIR%\convertRelease.pl -T %TOPLEVEL%\ %DLLDIR%\databaseModuleDirs.pm
+xcopy /Y /Q /D /R %BUILDIR%\*.pl %DLLDIR%\ > NUL
+xcopy /Y /Q /D /R %BUILDIR%\*.pm %TOPLEVEL%\lib\perl > NUL
+xcopy /Y /Q /D /R %BUILDIR%\DBD\*.pm %TOPLEVEL%\lib\perl\DBD > NUL
+xcopy /Y /Q /D /R %BUILDIR%\EPICS\*.pm %TOPLEVEL%\lib\perl\EPICS > NUL
---
+++ support/sscan/sscanApp/src/Build_POST.bat
@@ -0,0 +1,1 @@
+xcopy /Y /Q /D /R  O.Common\*.h  %TOPLEVEL%\include > NUL
