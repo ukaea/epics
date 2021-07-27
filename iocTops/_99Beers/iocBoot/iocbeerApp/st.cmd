39bd08e6ffe0b32d349be2b8d224b84c311f2c3b
tim-speight <60938034+tim-speight@users.noreply.github.com>
2021-07-27 22:51:02 +0100

    evening push

--- iocTops/_99Beers/iocBoot/iocbeerApp/st.cmd
+++ iocTops/_99Beers/iocBoot/iocbeerApp/st.cmd
@@ -1,4 +1,4 @@
-#!../../bin/linux-x86_64/bottles
+#!../../bin/windows-x64-debug/beers
 
 ## You may have to change bottles to something else
 ## everywhere it appears in this file
@@ -8,18 +8,12 @@
 cd "${TOP}"
 
 ## Register all support components
-dbLoadDatabase "dbd/bottles.dbd"
-bottles_registerRecordDeviceDriver pdbbase
+dbLoadDatabase "db/beers.dbd"
+beers_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
 dbLoadTemplate "db/user.substitutions"
-dbLoadRecords "db/dbSubExample.db", "user=ktn98257"
-
-## Set this to see messages from mySub
-#var mySubDebug 1
-
-## Run this to trace the stages of iocInit
-#traceIocInit
+dbLoadRecords "db/beer.db", "user=ktn98257"
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
