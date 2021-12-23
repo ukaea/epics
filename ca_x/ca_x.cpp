ae35c1af1b7a10b926a751459bc44e044c669588
Steve Terepin <10615120+k563@users.noreply.github.com>
2021-12-23 12:55:47 +0000

    Added 'ca_x' project

---
+++ ca_x/ca_x.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "ca_x.h"
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+
+#if 0
+#include "registryFunction.h"
+#include "epicsThread.h"
+#include "epicsExit.h"
+#include "epicsStdio.h"
+#include "dbStaticLib.h"
+#include "subRecord.h"
+#include "dbAccess.h"
+#include "asDbLib.h"
+#include "iocInit.h"
+#include "iocsh.h"
+#include "epicsInstallDir.h"
+#endif
---
+++ ca_x/ca_x.h
@@ -0,0 +1,7 @@
+#pragma once
+
+
+class ca_x
+{
+} ;
+
