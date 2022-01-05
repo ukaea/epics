93de1edd7fe1932f21c2f278094ef996f5014779
Steve Terepin <10615120+k563@users.noreply.github.com>
2022-01-05 18:22:06 +0000

    Safety copy

--- TestThinIoc_ConsoleApp/Program.cs
+++ TestThinIoc_ConsoleApp/Program.cs
@@ -9,15 +9,68 @@ namespace TestThinIoc_ConsoleApp
 
   //
   // The 'thin_ioc' project builds the dll to x64\Debug_DLL.
-  // This dll has to be manually copied to the console app project directory,
-  // alongside this program (together with the support dll's).
-  // These all get copied to the same directory as our EXE.
+  // This dll (together with the necessary support dll's) has to be
+  // manually copied to the console app project directory, alongside Program.cs.
+  // These DLL's need to be configured as 'Copy to output directory'.
+  // so that they'll get copied to the same directory as our EXE.
   //
   // To enable single-stepping into the DLL code :
   // - copy the .pdb file alongside the .dll
   //   [ note that this doesn't have to be copied to the output directory ]
   // - in the C# project properties, check 'enable native debugging'
   // https://stackoverflow.com/questions/21996641/how-to-step-into-p-invoked-c-code
+  //
+
+  //
+  // To build and run this mess :
+  //   COMPILE thin-ioc.cpp
+  //   BUILD the thin_ioc project
+  //   Copy thin_ioc.* from x64/Debug_DLL to /TestThinIoc_ConsoleApp
+  //   Rebuild the TestThinIoc_ConsoleApp project, so that the DLL's get copied to the output directory
+  //   Run the TestThinIoc_ConsoleApp
+  //
+
+  //
+  // Default Marshaling for Arrays - .NET Framework | Microsoft Docs
+  // https://docs.microsoft.com/en-us/dotnet/framework/interop/default-marshaling-for-arrays
+  // Platform Invoke (P/Invoke) | Microsoft Docs
+  // https://docs.microsoft.com/en-us/dotnet/standard/native-interop/pinvoke
+  //
+
+  //
+  // Exporting C Functions for Use in C or C++ Language Executables
+  // https://docs.microsoft.com/en-us/cpp/build/exporting-c-functions-for-use-in-c-or-cpp-language-executables?view=msvc-170
+  // 
+  // DLL Export Viewer
+  // https://www.nirsoft.net/utils/dll_export_viewer.html
+  // 
+  // Native DLL export functions viewer
+  // https://newbedev.com/is-there-any-native-dll-export-functions-viewer
+  //
+  // Exporting from a DLL Using __declspec(dllexport)
+  // https://docs.microsoft.com/en-us/cpp/build/exporting-from-a-dll-using-declspec-dllexport?view=msvc-170
+  //
+  // A Journey Through a P/Invoke Call
+  // https://www.jacksondunstan.com/articles/5120
+  //
+
+  //
+  // How to Debug a Release Build
+  // https://docs.microsoft.com/en-us/cpp/build/how-to-debug-a-release-build?view=msvc-170
+  //
+
+  //
+  // How can I specify a [DllImport] path at runtime ? *****************
+  // https://stackoverflow.com/questions/8836093/how-can-i-specify-a-dllimport-path-at-runtime
+  // It's the native Win32 DLL loading rules that govern things, regardless of whether you're using
+  // the handy managed wrappers (the P/Invoke marshaller just calls LoadLibrary). Those rules are enumerated
+  // in great detail here :
+  // https://docs.microsoft.com/en-gb/windows/win32/dlls/dynamic-link-library-search-order?redirectedfrom=MSDN
+  // The default search order will start looking in the directory from which your application was loaded.
+  // If you place the DLL there during the install, it will be found.
+  // If you need to force the application to look in a different directory for the DLL,
+  // you can modify the default search path using the SetDllDirectory function from Kernel32.
+  // https://docs.microsoft.com/en-gb/windows/win32/api/winbase/nf-winbase-setdlldirectorya
   //
 
   public class Program
@@ -28,6 +81,8 @@ namespace TestThinIoc_ConsoleApp
 
       System.Console.WriteLine("Invoking 'thin_ioc'") ;
 
+      SetDllDirectory("C:\\Users\\steve\\source\\repos\\epics.dotnet\\x64\\Debug_DLL") ;
+
       {
         // Just testing that we can invoke DLL functions
         int x = thin_ioc_func() ;
@@ -37,30 +92,50 @@ namespace TestThinIoc_ConsoleApp
         int div = thin_ioc_div(12,3) ;
       }
 
+     run:
       {
         var dbDescriptors = new DbDescriptor[]{
-          new DbDescriptor("db_A","macros_for_A"),
-          new DbDescriptor("db_B","macros_for_B"),
-          new DbDescriptor("db_C")
+          new DbDescriptor("C:\\tmp\\xx.db"),
+          //new DbDescriptor("db_B","macros_for_B"),
+          //new DbDescriptor("db_C")
         } ;
-        int x = thin_ioc_start(
-          "pathToDbdFile",
-          dbDescriptors,
-          dbDescriptors.Length,
-          "pathToCmdFile"
+        thin_ioc_set_is_running() ;
+        System.Threading.Tasks.Task.Run(
+          () => {
+            int x = thin_ioc_start(
+              dbDescriptors,
+              dbDescriptors.Length
+              // "pathToCmdFile",
+              // "pathToDbdFile"
+            ) ;
+          }
         ) ;
       }
 
-      System.Threading.Tasks.Task.Run(
-        () => {
-          int started = thin_ioc_start_xx(
-            nSecs : 60
-          ) ;
-        }
-      ) ;
 
-      System.Console.WriteLine("Waiting ...") ;
+
+      // System.Threading.Tasks.Task.Run(
+      //   () => {
+      //     int started = thin_ioc_start_xx(
+      //       nSecs : 60
+      //     ) ;
+      //   }
+      // ) ;
+
+      while ( thin_ioc_is_running() != 0 )
+      {
+        System.Console.WriteLine("Waiting ...") ;
+        string? line = System.Console.ReadLine() ;
+        if ( line?.StartsWith("x") == true )
+        {
+          thin_ioc_request_stop() ;
+        }
+      }
+      System.Console.WriteLine("Stopped ...") ;
       System.Console.ReadLine() ;
+
+      goto run ;
+
     }
 
     // In VS2022 command prompt :
@@ -70,6 +145,9 @@ namespace TestThinIoc_ConsoleApp
       // @"C:\Users\steve\source\repos\epics.dotnet\x64\thin_ioc" 
       "thin_ioc" 
     ) ;
+
+    [System.Runtime.InteropServices.DllImport("Kernel32")]
+    static extern bool SetDllDirectory ( [MarshalAs(UnmanagedType.LPStr)] string pathToSearch ) ;
 
     [System.Runtime.InteropServices.DllImport(THIN_IOC_DLL_path)]
     static extern short thin_ioc_add ( short a, short b ) ;
@@ -101,17 +179,20 @@ namespace TestThinIoc_ConsoleApp
 
     [System.Runtime.InteropServices.DllImport(THIN_IOC_DLL_path)]
     static extern int thin_ioc_start ( 
-      [MarshalAs(UnmanagedType.LPStr)] string?        pathToDbdFile,
       [In]                             DbDescriptor[] dbDescriptors,
       int                                             nDbDescriptors,
-      [MarshalAs(UnmanagedType.LPStr)] string?        pathToCmdFile
+      [MarshalAs(UnmanagedType.LPStr)] string?        pathToCmdFile = null,
+      [MarshalAs(UnmanagedType.LPStr)] string?        pathToDbdFile = null
     ) ;
 
     [System.Runtime.InteropServices.DllImport(THIN_IOC_DLL_path)]
-    static extern short thin_ioc_is_running ( ) ;
+    static extern int thin_ioc_is_running ( ) ;
 
     [System.Runtime.InteropServices.DllImport(THIN_IOC_DLL_path)]
-    static extern short thin_ioc_request_stop ( ) ;
+    static extern void thin_ioc_set_is_running ( ) ;
+
+    [System.Runtime.InteropServices.DllImport(THIN_IOC_DLL_path)]
+    static extern void thin_ioc_request_stop ( ) ;
 
     public static string ca_version ( )
     {
--- base-3.15.9/src/std/softIoc/thin_ioc.cpp
+++ base-3.15.9/src/std/softIoc/thin_ioc.cpp
@@ -22,6 +22,9 @@
 
 extern "C" int softIoc_registerRecordDeviceDriver(struct dbBase *pdbbase) ;
 
+// ???????????????????
+// extern "C" int base_registerRecordDeviceDriver(struct dbBase *pdbbase) ;
+
 #define DBD_FILE EPICS_BASE  "dbd\\softIoc.dbd"
 
 // #define EXIT_FILE EPICS_BASE "db\\softIocExit.db"
@@ -74,20 +77,99 @@ struct DbDescriptor
   char * Macros ;
 } ;
 
+// Cheap and cheerful flags that will be accessed by the C# thread
+
+static volatile int _is_running     = 1 ;
+
+static volatile int _stop_requested = 0 ;
+
 extern "C" __declspec(dllexport) 
 int thin_ioc_start(
-	char *         pathToDbdFile,
 	DbDescriptor * dbDescriptors,
 	int            nDbDescriptors,
-	char *         pathToCmdFile
+	char *         pathToCmdFile,
+	char *         pathToDbdFile
 ) {
+	fprintf(epicsGetStdout(),"THIN_IOC STARTING\n") ;
+	if ( pathToDbdFile == NULL )
+	{
+	  // Hmm, using 'base.dbd' instead of 'softIoc.dbd'
+		// seems to work, but maybe not a good idea as
+		// we have to rely on calling 'softIoc_registerRecordDeviceDriver'
+	  // pathToDbdFile = EPICS_BASE "dbd\\base.dbd" ;
+	  pathToDbdFile = EPICS_BASE "dbd\\softIoc.dbd" ;
+	}
+	// Note that if we use 'base.dbd' instead of 'softIoc.dbd'
+	// it's necessary to specify the 'path' argument, otherwise
+	// the 'includes' are not found ...
+	const char * path = EPICS_BASE "dbd\\" ;
+  if ( dbLoadDatabase(base_dbd,path,NULL) != 0 ) 
+	{
+    return -1 ;
+	}
+	// Hmm, this code is generated from 'softIoc' ... DISCUSS_WITH_TINE
+	// CALLING IT IS NECESSARY, OTHERWISE NOTHING WORKS ...
+  softIoc_registerRecordDeviceDriver(pdbbase) ;
+  // base_registerRecordDeviceDriver(pdbbase) ; // ????????????????
   for ( int iDbDescriptor = 0 ; iDbDescriptor < nDbDescriptors ; iDbDescriptor++ )
 	{
+	  DbDescriptor & dbDescriptor = dbDescriptors[iDbDescriptor] ;
+		char * dbFilePath = dbDescriptor.PathToDbFile ;
+		char * macros     = dbDescriptor.Macros ;
+		if ( dbLoadRecords(dbFilePath,macros) != 0 ) 
+		{
+			return iDbDescriptor + 1 ;
+		}
 	}
-	return 123 ;
+	if ( iocInit() != 0 )
+	{
+	  return -2 ;
+	}
+	epicsThreadSleep(0.2) ;
+	if ( pathToCmdFile != NULL )
+	{
+	  if ( iocsh(pathToCmdFile) ) 
+		{
+		  epicsExit(EXIT_FAILURE) ;
+			return -3 ;
+		}
+	}
+	epicsThreadSleep(0.2) ;
+
+	// _is_running = 1 ;
+	int iSecs = 1 ;
+	do
+	{
+	  epicsThreadSleep(1.0) ; 
+	  fprintf(epicsGetStdout(),"THIN_IOC Running %d\n",iSecs++) ;
+	}
+	while ( 
+	  _stop_requested == 0
+  ) ;
+	fprintf(epicsGetStdout(),"THIN_IOC STOPPING\n") ;
+	// Hmm, this KILLS THE ENTIRE PROCESS !!!
+	// epicsExit(EXIT_SUCCESS) ;
+  _is_running = 0 ;
+	return 0 ;
 }
 
+extern "C" __declspec(dllexport) 
+int thin_ioc_is_running ( )
+{
+	return _is_running ;
+}
 
+extern "C" __declspec(dllexport) 
+void thin_ioc_set_is_running ( )
+{
+	_is_running = 1 ;
+}
+
+extern "C" __declspec(dllexport) 
+void thin_ioc_request_stop ( )
+{
+	_stop_requested = 1 ;
+}
 
 #if 0
 
